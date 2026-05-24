2d7b930ddc88afb5af0142d9bce1df72efc543cd
Aadarsh <aadarshsme@gmail.com>
2026-05-24 19:54:06 +0530

    fix: add production frontend URL to CORS config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://frontend-production-00d0.up.railway.app',
+    ]
+
   });
 
   app.setGlobalPrefix('api');
